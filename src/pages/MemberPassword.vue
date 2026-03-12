219e611f7bc0b983708341abb957fa8bf172949a
CC Sun <toletgo101214@gmail.com>
2026-03-12 19:09:27 +0800

    Update layout for MemberPassword and MemberProfile pages below md

--- src/pages/MemberPassword.vue
+++ src/pages/MemberPassword.vue
@@ -8,17 +8,17 @@
             <h1 class="text-3xl text-center tracking-[2rem] pl-[2rem] text-base-heavy">會員密碼</h1>
         </div>
         <div class="max-w-[80%] w-full mx-auto flex flex-col gap-8">
-            <!-- <p class="font-bold text-2xl text-base-heavy">哩賀！甲奔未！{{ userInfo.name }}！！！</p> -->
             <div class="text-base-heavy">
                 <div class="flex flex-row gap-4 ps-2 pb-4 items-center">
                     <i class="fa-solid fa-user"></i>
                     <p class="font-bold text-lg text-base-heavy">會員密碼</p>
                 </div>
-                <div class="bg-white flex justify-center p-8">
-                    <form class="bg-white px-10 py-18 w-full max-w-3xl flex flex-col gap-6 justify-center">
+                <div class="bg-white flex justify-center">
+                    <form
+                        class="bg-white md:px-10 px-8 md:py-18 py-12 w-full max-w-3xl flex flex-col gap-6 justify-center">
                         <div class="flex flex-col gap-4">
-                            <div class="flex flex-col gap-1 px-4">
-                                <div class="flex flex-row w-full items-center">
+                            <div class="flex flex-col gap-1">
+                                <div class="flex md:flex-row flex-col md:items-center item-start w-full">
                                     <label for="usernameInput"
                                         class="inline-block w-28 font-bold text-base-light">舊密碼<span
                                             class="text-red-500">*</span></label>
@@ -31,8 +31,8 @@
                                     isError.oldPassword }}</small>
                             </div>
                             <div class="w-full h-px bg-gray-100"></div>
-                            <div class="flex flex-col gap-1 px-4">
-                                <div class="flex flex-row w-full items-center">
+                            <div class="flex flex-col gap-1">
+                                <div class="flex md:flex-row flex-col md:items-center item-start w-full">
                                     <label for="usernameInput"
                                         class="inline-block w-28 font-bold text-base-light">新密碼<span
                                             class="text-red-500">*</span></label>
@@ -45,8 +45,8 @@
                                     isError.newPassword }}</small>
                             </div>
                             <div class="w-full h-px bg-gray-100"></div>
-                            <div class="flex flex-col gap-1 px-4">
-                                <div class="flex flex-row w-full items-center">
+                            <div class="flex flex-col gap-1">
+                                <div class="flex md:flex-row flex-col md:items-center item-start w-full">
                                     <label for="realNameInput"
                                         class="inline-block w-28 font-bold text-base-light">確認新密碼<span
                                             class="text-red-500">*</span></label>
@@ -59,17 +59,14 @@
                                 <small v-if="isError.checkedPassword" class="text-sm text-end text-hot-red">{{
                                     isError.checkedPassword }}</small>
                             </div>
-                            <div class="flex gap-4 justify-center items-center">
+                            <div class="flex gap-4 justify-center items-center md:py-0 py-4">
                                 <button type="submit" @click.prevent="changePassword()"
-                                    class="w-36 cursor-pointer bg-hot-red text-white py-3  hover:bg-red-500 active:bg-red-700 transition-colors">確認變更</button>
+                                    class="md:w-36 w-full cursor-pointer bg-hot-red text-white py-3 hover:bg-red-500 active:bg-red-700 transition-colors">確認變更</button>
                                 <router-link to="/member"
-                                    class="w-36 cursor-pointer bg-gray-400 text-white text-center py-3  hover:bg-gray-300 active:bg-gray-500 transition-colors">返回</router-link>
+                                    class="md:w-36 w-full cursor-pointer bg-gray-400 text-white text-center py-3  hover:bg-gray-300 active:bg-gray-500 transition-colors">返回</router-link>
                             </div>
                             <div class="w-full h-px bg-gray-100"></div>
-                            <div>
-                                <p class="text-xs px-4 text-red-500">* 為必填欄位。</p>
-                                <p class="text-xs px-4 text-red-500">* 務必確認證件及護照相關資料，否則將可能損失購票權益。</p>
-                            </div>
+                            <p class="text-xs px-4 text-red-500">* 為必填欄位。</p>
                         </div>
                     </form>
                 </div>
--- src/pages/MemberProfile.vue
+++ src/pages/MemberProfile.vue
@@ -14,11 +14,11 @@
                     <i class="fa-solid fa-user"></i>
                     <p class="font-bold text-lg text-base-heavy">會員資料</p>
                 </div>
-                <div class="bg-white flex justify-center p-8">
-                    <form class="bg-white px-10 py-18 w-full max-w-3xl flex flex-col gap-6 justify-center">
+                <div class="bg-white flex justify-center">
+                    <form class="bg-white md:px-10 px-8 md:py-18 py-12 w-full max-w-3xl flex flex-col gap-6 justify-center">
                         <div class="flex flex-col gap-4">
-                            <div class="flex flex-col gap-1 px-4">
-                                <div class="flex flex-row w-full items-center items-center">
+                            <div class="flex flex-col gap-1">
+                                <div class="flex md:flex-row flex-col md:items-center item-start w-full">
                                     <label for="emailInput"
                                         class="inline-block w-28 font-bold text-base-light">Email</label>
                                     <input id="emailInput" type="email" v-model.trim="userInfo.email"
@@ -27,8 +27,8 @@
                                 </div>
                             </div>
                             <div class="w-full h-px bg-gray-100"></div>
-                            <div class="flex flex-col gap-1 px-4">
-                                <div class="flex flex-row w-full items-center">
+                            <div class="flex flex-col gap-1">
+                                <div class="flex md:flex-row flex-col md:items-center item-start w-full">
                                     <label for="usernameInput"
                                         class="inline-block w-28 font-bold text-base-light">會員名稱<span
                                             class="text-red-500">*</span></label>
@@ -41,8 +41,8 @@
                                     isError.name }}</small>
                             </div>
                             <div class="w-full h-px bg-gray-100"></div>
-                            <div class="flex flex-col gap-1 px-4">
-                                <div class="flex flex-row w-full items-center">
+                            <div class="flex flex-col gap-1">
+                                <div class="flex md:flex-row flex-col md:items-center item-start w-full">
                                     <label for="realNameInput"
                                         class="inline-block w-28 font-bold text-base-light">真實姓名</label>
                                     <input id="realNameInput" type="text" v-model.trim="userInfo.realName"
@@ -54,8 +54,8 @@
                                     isError.realName }}</small>
                             </div>
                             <div class="w-full h-px bg-gray-100"></div>
-                            <div class="flex flex-col gap-1 px-4">
-                                <div class="flex flex-row w-full items-center">
+                            <div class="flex flex-col gap-1">
+                                <div class="flex md:flex-row flex-col md:items-center item-start w-full">
                                     <label for="passportNameInput"
                                         class="inline-block w-28 font-bold text-base-light">護照名稱</label>
                                     <input id="passportNameInput" type="text" v-model.trim="userInfo.passportName"
@@ -68,8 +68,8 @@
                                     isError.passportName }}</small>
                             </div>
                             <div class="w-full h-px bg-gray-100"></div>
-                            <div class="flex flex-col gap-1 px-4">
-                                <div class="flex flex-row w-full items-center">
+                            <div class="flex flex-col gap-1">
+                                <div class="flex md:flex-row flex-col md:items-center item-start w-full">
                                     <label for="passportNumberInput"
                                         class="inline-block w-28 font-bold text-base-light">護照號碼</label>
                                     <input id="passportNumberInput" type="text" v-model.trim="userInfo.passportNumber"
@@ -81,8 +81,8 @@
                                     isError.passportNumber }}</small>
                             </div>
                             <div class="w-full h-px bg-gray-100"></div>
-                            <div class="flex flex-col gap-1 px-4">
-                                <div class="flex flex-row w-full items-center">
+                            <div class="flex flex-col gap-1">
+                                <div class="flex md:flex-row flex-col md:items-center item-start w-full">
                                     <label for="idNumberInput"
                                         class="inline-block w-28 font-bold text-base-light">身分證號碼</label>
                                     <input id="idNumberInput" type="text" v-model.trim="userInfo.idNumber"
@@ -95,8 +95,8 @@
                                     isError.idNumber }}</small>
                             </div>
                             <div class="w-full h-px bg-gray-100"></div>
-                            <div class="flex flex-col gap-1 px-4">
-                                <div class="flex flex-row w-full items-center">
+                            <div class="flex flex-col gap-1">
+                                <div class="flex md:flex-row flex-col md:items-center item-start w-full">
                                     <label for="birthdayInput"
                                         class="inline-block w-28 font-bold text-base-light">出生日期</label>
                                     <div class="relative w-full"
@@ -118,8 +118,8 @@
                                     isError.birthDate }}</small>
                             </div>
                             <div class="w-full h-px bg-gray-100"></div>
-                            <div class="flex flex-col gap-1 px-4">
-                                <div class="flex flex-row w-full items-center">
+                            <div class="flex flex-col gap-1">
+                                <div class="flex md:flex-row flex-col md:items-center item-start w-full">
                                     <label for="phoneInput"
                                         class="inline-block w-28 font-bold text-base-light">手機號碼<span
                                             class="text-red-500">*</span></label>
@@ -132,8 +132,8 @@
                                     isError.phone }}</small>
                             </div>
                             <div class="w-full h-px bg-gray-100"></div>
-                            <div class="flex flex-col gap-1 px-4">
-                                <div class="flex flex-row w-full items-center">
+                            <div class="flex flex-col gap-1">
+                                <div class="flex md:flex-row flex-col md:items-center item-start w-full">
                                     <label for="addressInput"
                                         class="inline-block w-28 font-bold text-base-light">地址</label>
                                     <input id="addressInput" type="text" v-model.trim="userInfo.address"
@@ -145,11 +145,11 @@
                                     isError.address }}</small>
                             </div>
                         </div>
-                        <div class="flex gap-4 justify-center items-center">
+                        <div class="flex gap-4 justify-center items-center md:py-0 py-4">
                             <button type="submit" @click.prevent="patchUser()"
-                                class="w-36 cursor-pointer bg-hot-red text-white py-3  hover:bg-red-500 active:bg-red-700 transition-colors">確認變更</button>
+                                class="md:w-36 w-full cursor-pointer bg-hot-red text-white py-3  hover:bg-red-500 active:bg-red-700 transition-colors">確認變更</button>
                             <router-link to="/member"
-                                class="w-36 cursor-pointer bg-gray-400 text-white text-center py-3  hover:bg-gray-300 active:bg-gray-500 transition-colors">返回</router-link>
+                                class="md:w-36 w-full cursor-pointer bg-gray-400 text-white text-center py-3  hover:bg-gray-300 active:bg-gray-500 transition-colors">返回</router-link>
                         </div>
                         <div class="w-full h-px bg-gray-100"></div>
                         <div>
