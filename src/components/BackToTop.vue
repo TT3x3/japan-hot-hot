23ee3f3c89804df66a239a4dcd3a7126590c08fa
CC Sun <toletgo101214@gmail.com>
2026-03-25 21:33:00 +0800

    Update UI

--- src/components/BackToTop.vue
+++ src/components/BackToTop.vue
@@ -1,7 +1,7 @@
 <template>
     <!-- top -->
     <div v-show="showTopBtn" @click="backToTop()"
-        class="fixed bottom-[10%] right-14 md:bg-white/30 bg-white/90 p-3 flex flex-col gap-1 justify-center items-center hover:bg-white/80 cursor-pointer shadow-md">
+        class="rounded-[50%] fixed bottom-[10%] right-14 md:bg-white/30 bg-white/90 p-3 flex flex-col gap-1 justify-center items-center hover:bg-white/80 cursor-pointer shadow-md">
         <i class="pt-2 pb-1 fa-solid fa-angle-up fa-xl text-gray-600"></i>
         <p class="text-xs text-gray-400">TOP</p>
     </div>
